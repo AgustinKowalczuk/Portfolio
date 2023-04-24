9d73114f5fb539ecfe2c6a4daabb642ef65e96b5
Agustin Kowalczuk <75805313+AgustinKowalczuk@users.noreply.github.com>
2023-04-23 22:44:29 -0300

    Add ref to Contact buttons

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -1,7 +1,7 @@
 import gmail from "../../Icons/gmail.png";
 import github from "../../Icons/github.png";
 import linkedin from "../../Icons/linkedin.png";
-import './ContactMe.style.css';
+import "./ContactMe.style.css";
 
 export default function ContactMe() {
   return (
@@ -9,13 +9,40 @@ export default function ContactMe() {
       <h1>Formas de contactar</h1>
       <ul className="Contact-options-container">
         <li>
-          <img className="img-logo" src={gmail} alt="Aqui va una red Social" />
+          <button
+            className="Button-Mailto"
+            onClick={(e) => {
+              window.location.href =
+                "mailto:Kowalczukagustin@gmail.com?Subject=Me%20gustaria%20contactar%20contigo.";
+            }}
+          >
+            <img
+              className="img-logo"
+              src={gmail}
+              alt="Aqui va una red Social"
+            />
+          </button>
         </li>
         <li>
-          <img className="img-logo" src={github} alt="Aqui va una red Social" />
+          <a href="https://github.com/AgustinKowalczuk" target="_blank">
+            <img
+              className="img-logo"
+              src={github}
+              alt="Aqui va una red Social"
+            />
+          </a>
         </li>
         <li>
-          <img className="img-logo" src={linkedin} alt="Aqui va una red Social"/>
+          <a
+            href="https://www.linkedin.com/in/agustinkowalczuk/"
+            target="_blank"
+          >
+            <img
+              className="img-logo"
+              src={linkedin}
+              alt="Aqui va una red Social"
+            />
+          </a>
         </li>
       </ul>
     </section>
